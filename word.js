cca7de31a480a16a1b7a9ef6442328eb37b32add
Greg Ladden <greg@gl-digital.com>
2017-10-22 15:13:14 -0500

    adding logic

---
+++ word.js
@@ -0,0 +1,27 @@
+
+var Letter = require("./Letter.js");
+
+// word constructor
+var Word = function(word) {
+  
+  this.word = word;
+
+  // swaps out underscores for correct letter(s)
+  Word.prototype.swapLetter = function(string, i, userGuess){
+    // console.log("blankRandomord.word: " + blankRandomWord.word);
+    var swappedLetter = string.substr(0, i * 2) + userGuess + string.substr(i * 2 + 1);
+    return swappedLetter;
+  }
+
+  // displays blanks for letters in word
+  Word.prototype.displayBlanks = function() {
+    var blankRandomWord = "";
+    for (var i = 0; i < word.length; i++) {
+      blankRandomWord += '_ ';
+    };
+    return blankRandomWord;  
+  }
+
+}
+
+module.exports = Word;
